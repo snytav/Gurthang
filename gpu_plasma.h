6df365990fc7fb69587c6ef1c565d4d2331785d9
Alexey Snytnikov <snytav@gmail.com>
2018-04-30 10:25:28 +0700

    removed reference to Cell in CheckArraySilent

--- gpu_plasma.h
+++ gpu_plasma.h
@@ -4498,19 +4498,19 @@ double CheckArray	(double* a, double* dbg_a)
 
 double CheckArraySilent	(double* a, double* dbg_a,int size)
 	{
-	    Cell<Particle> c = (*AllCells)[0];
+	   // Cell<Particle> c = (*AllCells)[0];
 	    double diff = 0.0;
 
 	    for(int n = 0;n < size;n++)
 	    {
             diff += pow(a[n] - dbg_a[n],2.0);
 
-	        if(fabs(a[n] - dbg_a[n]) > TOLERANCE)
-		    {
-
-		       int3 i = c.getCellTripletNumber(n);
-
-     		}
+//	        if(fabs(a[n] - dbg_a[n]) > TOLERANCE)
+//		    {
+//
+//		       int3 i = c.getCellTripletNumber(n);
+//
+//     		}
 	    }
 
 	    return pow(diff/(size),0.5);
