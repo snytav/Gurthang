332d966ccafef5333a0df30a3e5b07335d54e675
Alexey Snytnikov <snytav@gmail.com>
2018-04-30 17:13:33 +0700

    computation loop from main in Compute

--- gpu_plasma.h
+++ gpu_plasma.h
@@ -5837,6 +5837,43 @@ int memory_monitor(char *legend,int nt)
 }
 
 
+int Compute()
+{
+	   double t = compareCPUtoGPU();
+	   printf("----------------------------------------------------------- plasma check before move %.5f\n",t);
+	   size_t m_free,m_total;
+
+	   cudaMemGetInfo(&m_free,&m_total);
+
+	   struct sysinfo info;
+
+
+	   for(int nt = START_STEP_NUMBER;nt <= TOTAL_STEPS;nt++)
+	   {
+		   cudaMemGetInfo(&m_free,&m_total);
+		   sysinfo(&info);
+#ifdef MEMORY_PRINTS
+	       printf("before Step  %10d CPU memory free %10u GPU memory total %10d free %10d\n",
+	    		   nt,info.freeram/1024/1024,m_total/1024/1024,m_free/1024/1024);
+#endif
+
+	       Step(nt);
+
+	       cudaMemGetInfo(&m_free,&m_total);
+	       sysinfo(&info);
+#ifdef MEMORY_PRINTS
+	       printf("after  Step  %10d CPU memory free %10u GPU memory total %10d free %10d\n",
+	    		   nt,info.freeram/1024/1024/1024,m_total/1024/1024/1024,m_free/1024/1024/1024);
+#endif
+	   }
+
+
+	   t = compareCPUtoGPU();
+	   printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ plasma check after move %.5f\n",t);
+
+}
+
+
 };
 
 
--- main.cu
+++ main.cu
@@ -11,37 +11,7 @@ int main(int argc,char*argv[])
 
    plasma->Initialize();
 
-   double t = plasma->compareCPUtoGPU();
-   printf("----------------------------------------------------------- plasma check before move %.5f\n",t);
-   size_t m_free,m_total;
-
-   cudaMemGetInfo(&m_free,&m_total);
-
-   struct sysinfo info;
-
-
-   for(int nt = START_STEP_NUMBER;nt <= TOTAL_STEPS;nt++)
-   {
-	   cudaMemGetInfo(&m_free,&m_total);
-	   sysinfo(&info);
-#ifdef MEMORY_PRINTS
-       printf("before Step  %10d CPU memory free %10u GPU memory total %10d free %10d\n",
-    		   nt,info.freeram/1024/1024,m_total/1024/1024,m_free/1024/1024);
-#endif
-
-       plasma->Step(nt);
-
-       cudaMemGetInfo(&m_free,&m_total);
-       sysinfo(&info);
-#ifdef MEMORY_PRINTS
-       printf("after  Step  %10d CPU memory free %10u GPU memory total %10d free %10d\n",
-    		   nt,info.freeram/1024/1024/1024,m_total/1024/1024/1024,m_free/1024/1024/1024);
-#endif
-   }
-
-
-   t = plasma->compareCPUtoGPU();
-   printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ plasma check after move %.5f\n",t);
+   plasma->Compute();
 
    delete plasma;
    
