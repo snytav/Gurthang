e9160a90b09e8c5fc4b0e475d71f5e9e60cc41ed
Alexey Snytnikov <snytav@gmail.com>
2018-07-06 14:21:57 +0700

    saving new coords to Particle returned to Particle::Move

--- move.cu
+++ move.cu
@@ -29,15 +29,15 @@ Particle *Move(unsigned int i,int *cells,CurrentTensor *t1,CurrentTensor *t2,dou
    		     x1 = p.GetX1();
     		 q_m = p.GetQ2M();
     		 CurrentToMesh(x,x1,m,q_m,tau,cells,t1,t2,&p);
-//    		     		 p.x = x1.x;
-//    		     		 p.y = x1.y;
-//    		     		 p.z = x1.z;
-//
-//	         Reflect(&p);
-//
-//
-//
-//     writeParticleToSurface(i,&p);
+    		     		 p.x = x1.x;
+    		     		 p.y = x1.y;
+    		     		 p.z = x1.z;
+
+	         Reflect(&p);
+
+
+
+     writeParticleToSurface(i,&p);
 
      return (&p);
 }
