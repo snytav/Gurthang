e651a0388ecf932a22c51f1a8336fb639e4eb0d1
Alexey Snytnikov <snytav@gmail.com>
2018-07-06 13:51:02 +0700

    getfield returns value

--- cell.h
+++ cell.h
@@ -1244,10 +1244,11 @@ double getWrongCurrentAttribute(int num_along_cell,int num_attr)
  #ifdef __CUDACC__
  __host__ __device__
  #endif
-void GetField(double3 x,double3 & E,double3 & H,Particle *p,CellDouble *Ex1,CellDouble *Ey1,CellDouble *Ez1,CellDouble *Hx1,CellDouble *Hy1,CellDouble *Hz1)
+Field GetField(double3 x,Particle *p,CellDouble *Ex1,CellDouble *Ey1,CellDouble *Ez1,CellDouble *Hx1,CellDouble *Hy1,CellDouble *Hz1)
 {
     int3 i,i1;
 	double s1,s2,s3,s4,s5,s6,s11,s21,s31,s41,s51,s61;
+	Field fd;
 
 	    if(x.x < 0 || x.y < 0 || x.z < 0)
 	    {
@@ -1261,13 +1262,14 @@ void GetField(double3 x,double3 & E,double3 & H,Particle *p,CellDouble *Ex1,Cell
 
 
 
-        E = GetElectricField(i,i1,
+        fd.E = GetElectricField(i,i1,
 			     s1,s2,s3,s4,s5,s6,
 			     s11,s21,s31,s41,s51,s61,p,Ex1,Ey1,Ez1);
 
-	    H = GetMagneticField(i,i1,
+	    fd.H = GetMagneticField(i,i1,
 			     s1,s2,s3,s4,s5,s6,
 			     s11,s21,s31,s41,s51,s61,p,Hx1,Hy1,Hz1);
+	    return fd;
 }
 
 #ifdef __CUDACC__
--- move.cu
+++ move.cu
@@ -11,17 +11,18 @@ Particle *Move(unsigned int i,int *cells,CurrentTensor *t1,CurrentTensor *t2,dou
 		 CellDouble *Hx1,CellDouble *Hy1,CellDouble *Hz1)
 {
 
-     double3 x,x1,E,H;
+     double3 x,x1;
      double  m,q_m;
 //     int flag;
      Particle p;
+     Field fd;
 
      if(i >= number_of_particles) return 0;
      p = readParticleFromSurfaceDevice(i);
 //     jmp = jmp_control;
     		 x = p.GetX();
-    		 GetField(x,E,H,&p,Ex1,Ey1,Ez1,Hx1,Hy1,Hz1);
-    		 p.Move(E,H,tau);
+    		 fd = GetField(x,&p,Ex1,Ey1,Ez1,Hx1,Hy1,Hz1);
+    		 p.Move(fd.E,fd.H,tau);
     		 m = p.GetMass();
 
     		 x = p.GetX();
