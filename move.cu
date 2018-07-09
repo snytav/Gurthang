f9a6b4ca7cee291a90c810259d66f9f0fd16942a
Alexey Snytnikov <snytav@gmail.com>
2018-07-09 15:47:25 +0700

    clean move.cu

--- move.cu
+++ move.cu
@@ -12,33 +12,24 @@ void Move(unsigned int i,int *cells,CurrentTensor *t1,CurrentTensor *t2,double m
 {
      double3 x,x1;
      double  m,q_m;
-//     int flag;
      Particle p;
      Field fd;
 
      if(i >= number_of_particles) return;
      p = readParticleFromSurfaceDevice(i);
-//	 x = p.GetX();
 	 fd = GetField(&p,Ex1,Ey1,Ez1,Hx1,Hy1,Hz1);
 
 	 p.Move(fd.E,fd.H,tau);
 	 writeParticleToSurface(i,&p);
 
 	 Particle p1;
-//	 p1 = readParticleFromSurfaceDevice(i);
-//
-//	 CurrentToMesh(tau,cells,t1,t2,&p1);
-//
-//	 writeParticleToSurface(i,&p1);
-//	 AccCurrent(i,cells,t1,t2,mass,q_mass,Ex1,Ey1,Ez1,Hx1,Hy1,Hz1);
+
 }
 
 #ifdef __CUDACC__
  __host__ __device__
  #endif
  void AccCurrent(unsigned int i,int *cells,CurrentTensor *t1,CurrentTensor *t2,double mass,double q_mass)
-// 		 CellDouble *Ex1,CellDouble *Ey1,CellDouble *Ez1,
-// 		 CellDouble *Hx1,CellDouble *Hy1,CellDouble *Hz1)
  {
 	 Particle p;
 	 if(i >= number_of_particles) return;
@@ -46,10 +37,5 @@ void Move(unsigned int i,int *cells,CurrentTensor *t1,CurrentTensor *t2,double m
 	 p = readParticleFromSurfaceDevice(i);
 	 CurrentToMesh(tau,cells,t1,t2,&p);
 
-
-
-
      writeParticleToSurface(i,&p);
-
-//     return p;
 }
