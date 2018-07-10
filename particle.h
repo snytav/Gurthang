1683e74f42040ea7fc322ea203072144e607e56d
Alexey Snytnikov <snytav@gmail.com>
2018-07-10 10:07:48 +0700

    a.t[0] = b.t[0];

--- particle.h
+++ particle.h
@@ -18,13 +18,42 @@ typedef struct Field{
 } Field;
 
 
-typedef struct CurrentTensorComponent {
+class CurrentTensorComponent {
+public:
 	char i11, i12, i13,
 	 i21, i22, i23,
 	 i31, i32, i33,
 	 i41, i42, i43;
 	double t[4];
-} CurrentTensorComponent;
+};
+
+//CurrentTensorComponent & operator=(CurrentTensorComponent & b)
+//{
+//	CurrentTensorComponent a;
+//
+//	a.i11 = b.i11;
+//	a.i12 = b.i12;
+//	a.i13 = b.i13;
+//
+//	a.i21 = b.i21;
+//	a.i22 = b.i22;
+//	a.i23 = b.i23;
+//
+//	a.i31 = b.i31;
+//	a.i32 = b.i32;
+//	a.i33 = b.i33;
+//
+//	a.i41 = b.i41;
+//	a.i42 = b.i42;
+//	a.i43 = b.i43;
+//
+//	a.t[0] = b.t[0];
+//	a.t[1] = b.t[1];
+//	a.t[2] = b.t[2];
+//	a.t[3] = b.t[3];
+//
+//	return a;
+//}
 
 typedef struct CurrentTensor {
 	CurrentTensorComponent Jx,Jy,Jz;
