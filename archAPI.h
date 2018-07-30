7fe200febd7aea49f767211962484b3ee5259644
Alexey Snytnikov <snytav@gmail.com>
2018-07-30 19:08:22 +0700

    // KernelParams

--- archAPI.h
+++ archAPI.h
@@ -7,14 +7,7 @@
 
 
 
-//TODO: 1. set to par file a switch: use GPU (if any) or CPU
-//      2. check other versions, store them as brunches, add a switch if possible (and necessary)
-//      3. find Fortran-order pushing
-//      4. prepare for OMP profiling
-//      5. set measurement for time of everything
-//      6. set Berendeev beams
-//      7. add MPI
-//      8. FIND GPU RESULT CHECKING
+
 
 #ifndef ARCHAPI_H_
 #define ARCHAPI_H_
@@ -37,9 +30,7 @@
 extern uint3 threadIdx,blockIdx;
 #endif
 
-//#define DETAILED_CURRENTS_WRITE
-//#define OMP_DETAILED_OUTPUT
-//#define OMP_OUTPUT
+
 #define OMP_NUM_THREADS 100
 
 #ifndef __CUDACC__
@@ -78,52 +69,52 @@ typedef struct dim3 dim3;
 #endif
 
 
-typedef struct {
-	double d_ee; //electric energy
-	double *d_Ex,*d_Ey,*d_Ez; // electric field
-	double *d_Hx,*d_Hy,*d_Hz; // magnetic field
-	double *d_Jx,*d_Jy,*d_Jz; // currents
-	double *d_Rho;
-	int nt;                                 // timestep
-	int *d_stage;                           // checking system (e.g. for flow-out particles)
-	int *numbers;                            // number of particles in each cell
-	double mass,q_mass;
-	double *d_ctrlParticles;
-	int jmp;
-//	                                        for periodical FIELDS
-	int i_s,k_s;                        //
-	double *E;                              //the field
-	int dir;                                // the direction being processed
-	int to,from;                            // the range along the direction
-
-//	                                        for periodical CURRENTS
-	int dirE;                           // directions
-	int N;                           // variables
-
-//                                          electric field solver
-	int l_s;                        // variables
-	double *H1,*H2;                          // magnetic fields (orthogonal)
-	double *J;                              // current
-	double c1,c2,tau;                       //grid steps squared
-	int dx1,dy1,dz1,dx2,dy2,dz2;            //shifts
-
-	double B0;                               //constant magnetic field
-
-//	                                        magnetic field solver
-	double *Q;                              // magnetic field at half-step
-	double *H;                              // magnetic field
-	double *E1,*E2;                         // electric fields (orthogonal)
-	int particles_processed_by_a_single_thread;
-	unsigned int blockDim_x,blockDim_y,blockDim_z; // block for field solver
-
-	int component_total,sorts;               //for diagnostic particles
-	ParticleFloatArraysGroup *d_part_diag;
-
-	int *d_p_send_array;
-	int p_send_array_size;
-
-	double flown_beam_particles;
-} KernelParams;
+//typedef struct {
+//	double d_ee; //electric energy
+//	double *d_Ex,*d_Ey,*d_Ez; // electric field
+//	double *d_Hx,*d_Hy,*d_Hz; // magnetic field
+//	double *d_Jx,*d_Jy,*d_Jz; // currents
+//	double *d_Rho;
+//	int nt;                                 // timestep
+//	int *d_stage;                           // checking system (e.g. for flow-out particles)
+//	int *numbers;                            // number of particles in each cell
+//	double mass,q_mass;
+//	double *d_ctrlParticles;
+//	int jmp;
+////	                                        for periodical FIELDS
+//	int i_s,k_s;                        //
+//	double *E;                              //the field
+//	int dir;                                // the direction being processed
+//	int to,from;                            // the range along the direction
+//
+////	                                        for periodical CURRENTS
+//	int dirE;                           // directions
+//	int N;                           // variables
+//
+////                                          electric field solver
+//	int l_s;                        // variables
+//	double *H1,*H2;                          // magnetic fields (orthogonal)
+//	double *J;                              // current
+//	double c1,c2,tau;                       //grid steps squared
+//	int dx1,dy1,dz1,dx2,dy2,dz2;            //shifts
+//
+//	double B0;                               //constant magnetic field
+//
+////	                                        magnetic field solver
+//	double *Q;                              // magnetic field at half-step
+//	double *H;                              // magnetic field
+//	double *E1,*E2;                         // electric fields (orthogonal)
+//	int particles_processed_by_a_single_thread;
+//	unsigned int blockDim_x,blockDim_y,blockDim_z; // block for field solver
+//
+//	int component_total,sorts;               //for diagnostic particles
+//	ParticleFloatArraysGroup *d_part_diag;
+//
+//	int *d_p_send_array;
+//	int p_send_array_size;
+//
+//	double flown_beam_particles;
+//} KernelParams;
 
 #ifdef __CUDACC__
 #define COPY_FUNCTION_POINTER(dst,src) 	cudaMemcpyFromSymbol( (dst),(src), sizeof( SingleNodeFunctionType ) );
